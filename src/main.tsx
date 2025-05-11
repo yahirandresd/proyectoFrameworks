ca4021e65c60eec84d6dfea87ef29fe45b59c72a
jesucrist0 <jesus.milian46632@ucaldas.edu.co>
2025-05-10 19:23:23 -0500

    google

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import './satoshi.css';
+import { GoogleOAuthProvider } from '@react-oauth/google'
+const CLIENT_ID = '397391716664-e1hot3hhu14fr1fberotqisdg9ctb1uc.apps.googleusercontent.com'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <GoogleOAuthProvider clientId={CLIENT_ID}>
+       <App />
+      </GoogleOAuthProvider>
     </Router>
   </React.StrictMode>
 );
